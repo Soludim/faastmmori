360b9d5b809872044cbbeaee1cee64127f71e053
ISRAEL MANU DEBRAH <isrealmanudebrah@ISRAELs-MacBook-Pro.local>
2023-02-15 23:34:08 +0000

    added procfile

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -7,7 +7,7 @@
                     <div class="col-xl-3 col-lg-6 col-md-6 wow fadeInUp" data-wow-delay="100ms">
                         <div class="footer-widget__column footer-widget__about">
                             <div class="footer-widget__logo">
-                                <a href="index.html"><img src="../../images/resources/sahel-logo.png" alt=""></a>
+                                <a href="{{route('home')}}"><img src="../../images/resources/sahel-logo.png" alt=""></a>
                             </div>
                             <div class="footer-widget__about-text-box">
                                 <p class="footer-widget__about-text">Welcome to our Agriculture Farming.</p>
